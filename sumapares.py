e6d8031036136a4a1a31fab78267944224372ae1
Julian <juliancastillo0399@gmail.com>
2022-04-05 09:54:59 -0300

    Agrego algunas cosas en base a lo que vi en clases

--- sumapares.py
+++ sumapares.py
@@ -1,19 +1,43 @@
-from inspect import ArgSpec
+#from inspect import ArgSpec
+import sys
+import time
+import subprocess as sp
 import os
 import argparse
+from os import fork
+#from pty import fork
 
 def main():
     parser = argparse.ArgumentParser(description="Comandos")
     parser = argparse.ArgumentParser(description=False)
     parser.add_argument("-n", type=int,help="Numero de procesos hijos a generar")
-    parser.add_argument("-h", type=str, help="Mostrar ayuda")
-    parser.add_argument("-v", type=str, help="Activar modo verbose")
+    #parser.add_argument("-h", type=str, help="Mostrar ayuda")
+    parser.add_argument("-v", action="stare_true", help="Activar modo verbose")
     args = parser.parse_args()
-    fork_generator(args)
+    fork_generator(args.n,args.v)
+    os.wait()
 
-def fork_generator(args):
-    for i in(args.n):
+def fork_generator(args,verbose):
+    for i in(args):
         type_process = os.fork()
         if type_process == 0:
-            print("Proceso creado")
+            pid = os.getpid()
+            suma = 0
+            ppid = os.getppid()
+            print("Proceso iniciado ",pid)
+            if verbose:
+                print("Proceso iniciado ",pid)
+            for k in range (0, pid+1, 2):
+                suma = k + suma
+            if verbose:
+                print("Ending process ",pid)
+            print(pid, ppid, suma)
             os.exit(0)
+            
+            
+            
+            os._exit(0)
+        
+if __name__ == '__main__':
+    main()
+
