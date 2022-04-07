f28fbf98ada538d135d16f8ca468a3fa170281ca
Julian <juliancastillo0399@gmail.com>
2022-04-06 22:15:32 -0300

    Modifico sumapares.py y logro el funcionamiento correcto

--- sumapares.py
+++ sumapares.py
@@ -8,7 +8,7 @@ from os import fork
 def main():
     parser = argparse.ArgumentParser(description="Comandos")
     parser.add_argument("-n", "--number", type=int , required=True , help="Numero de procesos hijos a generar" )
-    #parser.add_argument("-h", type=str, help="Mostrar ayuda")
+    #parser.add_argument("-h", "--help", action="store_true", help="Mostrar ayuda")
     parser.add_argument("-v", "--verbose", action="store_true", help="Activar modo verbose")
     args = parser.parse_args()
     fork_generator(args.number,args.verbose)
@@ -17,22 +17,22 @@ def main():
 def fork_generator(number,verbose):
     for i in range(number):
         forkp = fork()
-        type_process = os.fork()
+        #type_process = os.fork()
         if forkp == 0:
             pid = os.getpid()
             suma = 0
             ppid = os.getppid()
             if verbose:
                 print("Proceso iniciado ",pid)
-            for k in range (0, pid+1, 2):
-                suma = k + suma
-            if verbose:
+                #time.sleep(1)
+                for k in range (0, pid+1, 2):
+                    suma = k + suma
+                time.sleep(0.5)
                 print("Proceso terminado ",pid)
-            print(pid, ppid, suma)
+                print(pid,"-",ppid,":",suma)
+            
             os._exit(0)
-            
-            
-                    
+
 if __name__ == '__main__':
     main()
 
