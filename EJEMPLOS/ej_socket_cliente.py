43ebd406a7af2475dbfb994b631494ceb34024f8
chuli99 <juliancastillo0399@gmail.com>
2022-09-02 22:14:02 -0300

    Ejercicio de Socket funcionando

--- EJEMPLOS/ej_socket_cliente.py
+++ EJEMPLOS/ej_socket_cliente.py
@@ -1,3 +1,11 @@
+#ENUNCIADO CLASE
+#Crear un server de mayusculas que atienda a varios clientes simultaneamente usando multiprocessing y/o threading.
+#Usar sockets TCP.
+#El servidor debe generar un proceso/hilo para atender aun cliente individual
+#El cliente debe poder enviar una palabra, el servidor respondera con la mayuscula de esa palabra.
+#Si el cliente envia un exit termina, y hace que el proceso/hilo que lo atendia del lado del server tambien termine
+
+
 import socket
 
 #Importamos si porque usamos los argumentos de linea de comandos cual es el host al que quiero conectarme
